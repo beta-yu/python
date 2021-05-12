dc696cb75104d26d26ad283852153aaf49fae18f
qiyu <qiyu56@qq.com>
2021-05-12 21:41:54 +0800

    add some notes.

---
+++ car.py
@@ -0,0 +1,14 @@
+class Car():
+
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0 # 设置了默认值，形参列表中不需要再包含
+
+    def get_descriptive_name(self):
+        long_name = str(self.year) + ' ' + self.make + ' ' + self.model
+        return long_name.title()
+
+    def read_odometer(self):
+        print("This car has " + str(self.odometer_reading) + " miles on it.")
